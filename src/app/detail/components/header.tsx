06c8677d405779c14431e59379244d9edb09a768
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-11-06 12:46:13 -0300

    ♻️ (header): Detail repo
    add dynamic title of header in detail repo

--- src/app/detail/components/header.tsx
+++ src/app/detail/components/header.tsx
@@ -1,7 +1,9 @@
-export default function Header() {
+export default function Header({ title }: {title: string}) {
   return (
     <header className="flex justify-center w-full p-5 bg-gray-100">
-      <h1 className="font-bold text-3xl uppercase">Repositories</h1>
+      <h1 className="font-bold text-3xl uppercase">
+        {title}
+      </h1>
     </header>
   )
 }
